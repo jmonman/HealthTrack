27ed67d07daee3ecaa594917e9c79b3b67193e0a
Joe <elder_joseph@bah.com>
2019-04-12 12:07:28 -0400

    Updates

---
+++ precache-manifest.aee35e1cc3c6266a390ffdc3f4690d85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "195b0251a0925bf8abf9",
+    "url": "/HealthTrack/static/css/main.8120a0cf.chunk.css"
+  },
+  {
+    "revision": "195b0251a0925bf8abf9",
+    "url": "/HealthTrack/static/js/main.3e37e6a3.chunk.js"
+  },
+  {
+    "revision": "f5996b6c7c1d40af057c",
+    "url": "/HealthTrack/static/js/runtime~main.2494de67.js"
+  },
+  {
+    "revision": "3a2c79a18e3ed29b8cf9",
+    "url": "/HealthTrack/static/js/2.1e027fe2.chunk.js"
+  },
+  {
+    "revision": "ef67b961f1111a9e1c96ca653a1153ae",
+    "url": "/HealthTrack/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/HealthTrack/precache-manifest.f1d04deea2934d0f7b830a9e0517361c.js"
+  "/HealthTrack/precache-manifest.aee35e1cc3c6266a390ffdc3f4690d85.js"
 );
 
 workbox.clientsClaim();
